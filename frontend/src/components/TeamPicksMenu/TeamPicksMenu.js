3ce5875ddb3f379da8b52f279629b6005fd39e64
cs_nshah001 <nsha001@odu.edu>
2022-04-17 17:59:22 -0400

    Start of scrollable list

--- frontend/src/components/TeamPicksMenu/TeamPicksMenu.js
+++ frontend/src/components/TeamPicksMenu/TeamPicksMenu.js
@@ -1,6 +1,27 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { ImageListItemBar, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 function TeamPicksMenu() {
+
     return (
-        <div>Scrollable List</div>
+        //<div>Scrollable List</div>
+        <CardActionArea component='a' href='#'>
+            <Card sx={{display: 'flex'}}>
+                <CardContent sx={{flex:1}}>
+                    <Typography component="h2" variant="h5">
+                        {"Pick 1:"}
+                    </Typography>
+                    <Typography variant="subtitle1" paragraph>
+                        {"Test Desc"}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </CardActionArea>
     );
 }
 
