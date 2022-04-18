4fcf60b8114cdaa11ae31245563d4d1d957d1120
Winston Shields <winstonjshields@gmail.com>
2022-04-17 23:04:20 -0400

    Update

--- frontend/src/components/TeamPicksMenu/TeamPicksMenu.js
+++ frontend/src/components/TeamPicksMenu/TeamPicksMenu.js
@@ -6,22 +6,42 @@ import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-function TeamPicksMenu() {
+
+function TeamPick(teamName, pickNumber) {
 
     return (
-        //<div>Scrollable List</div>
         <CardActionArea component='a' href='#'>
             <Card sx={{display: 'flex'}}>
                 <CardContent sx={{flex:1}}>
                     <Typography component="h2" variant="h5">
-                        {"Pick 1:"}
+                        {`Pick ${pickNumber}:`}
                     </Typography>
                     <Typography variant="subtitle1" paragraph>
-                        {"Test Desc"}
+                        {teamName}
                     </Typography>
                 </CardContent>
             </Card>
         </CardActionArea>
+    )
+}
+
+function TeamPicksMenu() {
+
+    var teams = ["Jags", "Lions", "Seahawks", "Bills"];
+
+    var teamPicks = teams.map((teamName, pickNumber) =>
+        <div style={{ display: "inline-block" }}>
+            {TeamPick(teamName, pickNumber)}
+        </div>
+    );
+
+    console.log(teamPicks)
+
+    return (
+        //<div>Scrollable List</div>
+        <div>
+            {teamPicks}
+        </div>
     );
 }
 
