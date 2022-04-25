d26326b3b97f43724e3b682e70ba5c9ba1265814
Winston Shields <winstonjshields@gmail.com>
2022-04-24 22:40:41 -0400

    recommneded players

--- frontend/src/components/RecommendedPlayers/RecommendedPlayers.js
+++ frontend/src/components/RecommendedPlayers/RecommendedPlayers.js
@@ -1,6 +1,35 @@
-function RecommendedPlayers() {
+import axios from "axios"
+import { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+
+function RecommendedPlayers(props) {
+
+    var team = props.team;
+    const host = window.location.hostname;
+    const port = 8000;
+
+    const [players, setPlayers] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        axios.get(`http://${host}:${port}/rec/${team.acryonm}`).then((res) => {
+            setPlayers(res.data)
+            setLoaded(true);
+            console.log(true)
+        })
+    }, []);
+
     return (
-        <div>Recommended Players</div>
+        <div>
+            Recommended Players
+            {
+                loaded ? players.map((player) =>
+                    <div>
+                        Name: {player.name} Height: {player.height} Position: {player.position} Position Grade: {player.positionGrade}
+                    </div>
+                ) : ""
+            }
+        </div>
     );
 }
 
